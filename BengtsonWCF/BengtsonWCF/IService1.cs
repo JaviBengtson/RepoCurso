1c6d583bbd953c4eeb6ed16e18b09f269e043009
Javier Bengtson <javierbengtson@gmail.com>
2023-10-09 01:30:20 -0300

    Agregando WCF

---
+++ BengtsonWCF/BengtsonWCF/IService1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BengtsonWCF
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        User GetUser(int value); 
+
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string Nombre{ get ;set; }
+
+        [DataMember]
+        public string Apellido{get; set;}
+    }
+}
---
+++ BengtsonWCF/BengtsonWCF/Service1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BengtsonWCF
+{
+    public class Service1 : IService1
+    {      
+
+        public User GetUser(int userId) 
+        {
+            return new User
+            {
+                Nombre = "Nose",
+                Apellido = "Martinez"
+
+            };
+        }
+
+	
+	}
+}
